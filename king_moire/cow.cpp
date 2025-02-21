ac4330ff18b0b42799e53e7b81a21ddbf6032417
Zooey <truezooe@gmail.com>
2025-02-21 00:47:27 -0600

    Cleanup and calques

--- king_moire/cow.cpp
+++ king_moire/cow.cpp
@@ -4,13 +4,12 @@
 #include <sstream>
 #include <cctype>
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <random>
 #include <format>
 #include <filesystem>
-
+#include <unordered_set>
+#include <set>
 
 /** My typedefs go here */
 using namespace std;
@@ -18,47 +17,44 @@ typedef int num_t;
 typedef vector<num_t> perm_t;
 typedef vector<perm_t> pa_t;
 
-
 /** Python library functions rewritten by ChatGPT go here. Forgive me */
 
 template <typename T>
-std::vector<std::vector<T>> combinations(const std::vector<T>& elements, int r) {
-    std::vector<std::vector<T>> result;
+vector<vector<T>> combinations(const vector<T>& elements, int r) {
+  vector<vector<T>> result;
 
-    int n = elements.size();
-    if (r > n) return result;  // Edge case: r cannot be greater than n
+  int n = elements.size();
+  if (r > n)
+    return result; // Edge case: r cannot be greater than n
 
-    // Binary selection mask: first r elements are 1 (selected), rest are 0
-    std::vector<bool> mask(n, false);
-    std::fill(mask.begin(), mask.begin() + r, true);
+  // Binary selection mask: first r elements are 1 (selected), rest are 0
+  vector<bool> mask(n, false);
+  fill(mask.begin(), mask.begin() + r, true);
 
-    do {
-        std::vector<T> combination;
-        for (int i = 0; i < n; i++) {
-            if (mask[i]) {
-                combination.push_back(elements[i]);
-            }
-        }
-        result.push_back(combination);
-    } while (std::prev_permutation(mask.begin(), mask.end()));
+  do {
+    vector<T> combination;
+    for (int i = 0; i < n; i++) {
+      if (mask[i]) {
+        combination.push_back(elements[i]);
+      }
+    }
+    result.push_back(combination);
+  } while (prev_permutation(mask.begin(), mask.end()));
 
-    return result;
+  return result;
 }
-
 
 perm_t listRange(const num_t start, const num_t end) {
   perm_t ret;
   for (num_t x = start; x < end; x++) {
     ret.push_back(x);
   }
-  return std::move(ret);
+  return move(ret);
 }
-
 
 perm_t listRange(const num_t end) {
   return listRange(0, end);
 }
-
 
 void printArray(const vector<vector<int>>& data) {
   for (const auto& row : data) {
@@ -69,21 +65,18 @@ void printArray(const vector<vector<int>>& data) {
   }
 }
 
-
 template <typename T>
-void randomShuffle(std::vector<T>& data) {
-    std::random_device rd;  // Seed source
-    std::mt19937 g(rd());   // Random number generator
-    std::shuffle(data.begin(), data.end(), g);
+void randomShuffle(vector<T>& data) {
+  random_device rd; // Seed source
+  mt19937 g(rd());  // Random number generator
+  shuffle(data.begin(), data.end(), g);
 }
 
-
-/** Helper functions */
-
+/** Helper functions for loading files*/
 
 pa_t enweave(pa_t const& A, const num_t n, const num_t d) {
   pa_t ret;
-  perm_t highs = listRange(n-d, n);
+  perm_t highs = listRange(n - d, n);
 
   for (perm_t row : A) {
     for (vector<int> ps : combinations(listRange(n), d)) {
@@ -92,7 +85,7 @@ pa_t enweave(pa_t const& A, const num_t n, const num_t d) {
       int h = 0;
       perm_t t;
       for (int i = 0; i < n; i++) {
-        if (std::find(ps.begin(), ps.end(), i) != ps.end()) {
+        if (find(ps.begin(), ps.end(), i) != ps.end()) {
           t.push_back(highs[h]);
           h += 1;
         } else {
@@ -105,7 +98,6 @@ pa_t enweave(pa_t const& A, const num_t n, const num_t d) {
   }
   return ret;
 }
-
 
 pa_t loadPa(const string& filename) {
   ifstream file(filename);
@@ -120,7 +112,7 @@ pa_t loadPa(const string& filename) {
   while (getline(file, line)) {
     perm_t row;
     stringstream ss;
-    
+
     // Extract numbers while ignoring non-numeric characters
     for (char ch : line) {
       if (ch == '#') {
@@ -145,28 +137,107 @@ pa_t loadPa(const string& filename) {
   return data;
 }
 
-
 pa_t loadPa2(const num_t n, const num_t d) {
   string filename = format("pa_{}_choose_{}_unfinished.txt", n, d);
   if (filesystem::exists(filename)) {
     printf("Loaded from unfinished file\n");
     return loadPa(filename);
   }
-  
-  filename = format("pa_{}_choose_{}_verified.txt", n-d, d);
+
+  filename = format("pa_{}_choose_{}_verified.txt", n - d, d);
   printf("Loaded from smaller file\n");
   return enweave(loadPa(filename), n, d);
 }
 
+/** Helper functions for manipulating PAs */
 
+vector<vector<vector<int>>> yoink_columns(const pa_t& A, int n, int d) {
+  int twists = n / d;
+  vector<vector<vector<int>>> ret(twists);
 
+  for (const auto& row : A) {
+    vector<vector<int>> buckets(twists);
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cerr << "Usage: " << argv[0] << " <filename>\n";
-    return 1;
+    for (size_t i = 0; i < row.size(); i++) {
+      int e = row[i];
+      buckets[e / d].push_back(i);
+    }
+
+    for (size_t j = 0; j < twists; j++) {
+      ret[j].push_back(buckets[j]);
+    }
   }
 
+  return ret;
+}
+
+inline bool separated(perm_t u, perm_t v, num_t d) {
+  num_t dd = d * d;
+  for (ssize_t i = 0; i < u.size(); i++) {
+    if (pow(u[i] - v[i], 2) >= dd) {
+      return true;
+    }
+  }
+  return false;
+}
+
+std::vector<std::unordered_set<int>> init_separations(const pa_t& A, int d) {
+  std::vector<std::unordered_set<int>> s(A.size());
+
+  for (size_t vx = 0; vx < A.size(); vx++) {
+    for (size_t ux = 0; ux < vx; ux++) {
+      if (ux != vx && separated(A[ux], A[vx], d)) {
+        s[ux].insert(vx);
+        s[vx].insert(ux);
+      }
+    }
+  }
+
+  return s;
+}
+
+perm_t apply_permutation(const perm_t& perm, const perm_t& src, const perm_t& dst) {
+  perm_t ret = perm;
+
+  for (size_t i = 0; i < src.size(); i++) {
+    ret[src[i]] = perm[dst[i]];
+  }
+
+  return ret;
+}
+
+void eval_permutation(
+  const pa_t& A,
+  const perm_t& src,
+  const perm_t& dst,
+  perm_t news, perm_t adders, perm_t subers,
+  std::vector<std::unordered_set<int>>& s,
+  int i, int d) {
+
+    news.clear();
+  adders.clear();
+  subers.clear();
+
+  perm_t pot = apply_permutation(A[i], src, dst);
+
+  for (size_t x = 0; x < A.size(); x++) {
+    if (x == i)
+      continue;
+
+    if (separated(pot, A[x], d)) {
+      if (s[i].find(x) == s[i].end()) {
+        news.push_back(x);
+      }
+      if (s[x].find(i) == s[x].end()) {
+        adders.push_back(x);
+      }
+    } else if (s[i].find(x) != s[i].end()) {
+      subers.push_back(x);
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
   // auto data = readNumbersFromFile(argv[1]);
   // cout << "Extracted Numbers:\n";
   auto data = loadPa2(9, 3);
