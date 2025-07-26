8674e5b6ef087b0e3bdb6b20924f987b6261e224
Zooey <truezooe@gmail.com>
2025-07-26 14:43:59 -0500

    Lazy hill - Fix writing/Ctrl+C race condition

--- july26/lazy_hill.py
+++ july26/lazy_hill.py
@@ -46,24 +46,30 @@ def infill(row, highs, ps):
   return new
 
 
+def symmetric_separated(u,v,n,d):
+  cu = symmetry(u, n)
+  cv = symmetry(v, n)
+  return separated(u,v,d) and separated(cu,v,d) and separated(u,cv,d) and separated(cu,cv,d)
+
+
 ############################### Special Utils
 
 
-# def complement(p, n):
-#   return [(e+n//2) % n for e in p]
-
-
-# def pull_groups(p, d):
-#   return tuple(e // d for e in p)
+def pull_groups(p, d):
+  return tuple(e // d for e in p)
 
 
 def resume_calculation(n, d):
   try:
     A = load_pa(f'pa_{n}_choose_{d}_lazy_bump_half_unfinished.txt')
+
     with open(f's_{n}_choose_{d}_lazy_bump_half_unfinished.txt', 'r') as f:
       s = list(map(set, json.load(f)))
+
+    with open(f'u_{n}_choose_{d}_lazy_bump_half_unfinished.txt', 'r') as f:
+      ups = set(json.load(f))
     print ('Loaded from unfinished file')
-    return A, s
+    return A, s, ups
   except Exception as e:
     print(e)
     pass
@@ -76,7 +82,7 @@ def resume_calculation(n, d):
       A = load_pa(f'pa_{n-d}_choose_{d}_verified.txt')
       print ('Loaded from smaller file')
     ret = enweave(A, n, d)
-    return ret, None
+    return ret, None, None
   except FileNotFoundError:
     print (f'No smaller file exists. Must create pa_{n-d}_choose_{d}_verified.txt')
     exit(1)
@@ -91,15 +97,16 @@ def enweave(A, n, d):
     for ps in it.combinations(list(range(n)), d):
       random.shuffle(highs)
       u = infill(row, highs, ps)
-      ret.append(u)
-
-      # v = complement(u, n)
-      # gu = pull_groups(u, d)
-      # gv = pull_groups(v, d)
-      # if gu not in seen and gv not in seen:
-      #   seen.add(gu)
-      #   seen.add(gv)
-      #   ret.append(u)
+      if symmetry:
+        v = symmetry(u, n)
+        gu = pull_groups(u, d)
+        gv = pull_groups(v, d)
+        if gu not in seen and gv not in seen:
+          seen.add(gu)
+          seen.add(gv)
+          ret.append(u)
+      else:
+        ret.append(u)
 
   return ret
 
@@ -121,7 +128,8 @@ def eval_permutation(A, n, pot, s, i, d):
   for x, e in enumerate(A):
     if x == i:
       continue
-    if separated(pot, e, d):
+    if (not cross_check and separated(pot, e, d) or
+        cross_check and symmetric_separated(pot, e, n, d)):
       if x not in s[i]:
         news.append(x)
       if i not in s[x]:
@@ -134,21 +142,23 @@ def eval_permutation(A, n, pot, s, i, d):
 def preval_separations(A, d, s, vx):
   v = A[vx]
   for ux, u in enumerate(A):
-    if ux != vx and separated(u, v, d):
+    if ux != vx and (not cross_check and separated(u, v, d) or
+        cross_check and symmetric_separated(u, v, n, d)):
       s[ux].add(vx)
       s[vx].add(ux)
   return s
 
 
-def hill_climb(A, n, d, s=None):
+def hill_climb(A, n, d, s=None, ups=None):
   N = len(A)
   s = s or [set() for _ in range(N)]
-  updated = [False]*N
+  updated = ups or set()
   W = N * (N-1)
 
   best_score = float('inf')
   best_pa = deepcopy(A)
   best_s = deepcopy(s)
+  best_u = deepcopy(updated)
   last_printed_score = float('inf')
   last_printed_it = -1
   last_tweak = 0
@@ -164,16 +174,17 @@ def hill_climb(A, n, d, s=None):
         should_print = True
         best_pa = deepcopy(A)
         best_s = deepcopy(s)
-        # if last_printed_score - best_score > 100 or (it_count - last_printed_it > 1000 and last_printed_score > best_score): 
-        #   last_printed_score = best_score
-        #   last_printed_it = it_count
-        #   yield deepcopy(best_pa), list(map(list, best_s))
+        best_u = deepcopy(updated)
+        if last_printed_score - best_score > 10000 or (it_count - last_printed_it > 1000 and last_printed_score > best_score):
+          last_printed_score = best_score
+          last_printed_it = it_count
+          yield deepcopy(best_pa), list(map(list, best_s)), list(best_u)
 
       if should_print:
-        print(datetime.now(), f'P({n},{d})', 'Iteration:', it_count, 'Score:', score, 'Best:', best_score, 'Coverage:', coverage, 'of', N, 'Last tweak:', last_tweak)
+        print(datetime.now(), f'P({n},{d})', 'Iteration:', it_count, 'Score:', score, 'Best:', best_score, 'Coverage:', coverage, 'of', N, 'Eval:', len(updated), 'Last tweak:', last_tweak)
 
       if N == coverage:
-        yield deepcopy(A), None
+        yield deepcopy(A), None, None
         return
 
       force = False
@@ -181,9 +192,9 @@ def hill_climb(A, n, d, s=None):
         A, s, last_tweak, force = deepcopy(best_pa), deepcopy(best_s), it_count, True
 
       i = random.randrange(N)
-      if not updated[i]:
+      if i not in updated:
         preval_separations(A, d, s, i)
-        updated[i] = True
+        updated.add(i)
 
       for tries in it.count():
         k = random.randrange(n//d)
@@ -192,9 +203,9 @@ def hill_climb(A, n, d, s=None):
         random.shuffle(dst)
         row = apply_permutation(A[i], src, dst)
 
-        # if not separated(row, complement(row, n), d):
-        #   # print('not separated')
-        #   continue
+        if symmetry and not separated(row, symmetry(row, n), d):
+          # print('not separated')
+          continue
 
         adders, subers, news = eval_permutation(A, n, row, s, i, d)
         if len(news) + len(adders) > 2*len(subers):
@@ -218,7 +229,7 @@ def hill_climb(A, n, d, s=None):
 
 
   except KeyboardInterrupt:
-    yield deepcopy(best_pa), list(map(list, best_s))
+    yield deepcopy(best_pa), list(map(list, best_s)), list(best_u)
     pass
 
 
@@ -230,35 +241,74 @@ def verify(pa, d):
   return True
 
 
+### Symmetries
+
+def complement(p, n):
+  return [n-1-e for e in p]
+
+
+def bump_half(p, n):
+  return [(e+n//2) % n for e in p]
+
+
 if __name__ == '__main__':
   from sys import argv
   n = int(argv[1])
   d = int(argv[2]) if len(argv) > 2 else n//4
 
+  # symmetry = None
+  # symmetry = complement
+  symmetry = bump_half
+
+  cross_check = symmetry in (complement,)
+
   # if len(argv) >= 3 and int(argv[2]) != d:
   #   print ('This program only works when d = n/4')
   #   exit(1)
 
-  A,s = resume_calculation(n, d)
-  for pot,pots in hill_climb(A, n, d, s):
+  A,s,ups = resume_calculation(n, d)
+  final_status = ''
+  for pot,pots,ups in hill_climb(A, n, d, s, ups):
     pa = list(pot)
-    # for row in pot:
-    #   pa.append(complement(row, n))
+    if symmetry:
+      for row in pot:
+        pa.append(complement(row, n))
 
-    if verify(pa, d):
-      filename = f'pa_{n}_choose_{d}_verified.txt'
-      print ('Verified', filename)
-      with open(filename, 'w+') as f:
-        for row in pa:
-          f.write(' '.join(map(str, row)) + '\n')
-    else:
-      filename = f'pa_{n}_choose_{d}_lazy_bump_half_unfinished.txt'
-      print ('Failed to verify', filename)
-      with open(filename, 'w+') as f:
-        for row in pot:
-          f.write(' '.join(map(str, row)) + '\n')
+    v = verify(pa, d)
+    # print('Writing')
+    exiting = False
 
-      filename = f's_{n}_choose_{d}_lazy_bump_half_unfinished.txt'
-      with open(filename, 'w+') as f:
-        json.dump(pots, f)
+    while True:
+      try:
+        if v:
+          filename = f'pa_{n}_choose_{d}_verified.txt'
+          final_status = f'Verified {filename}'
+          with open(filename, 'w+') as f:
+            for row in pa:
+              f.write(' '.join(map(str, row)) + '\n')
+        else:
+          filename = f'pa_{n}_choose_{d}_lazy_bump_half_unfinished.txt'
+          final_status = f'Failed to verify {filename}'
+          with open(filename, 'w+') as f:
+            for row in pot:
+              f.write(' '.join(map(str, row)) + '\n')
 
+          filename = f's_{n}_choose_{d}_lazy_bump_half_unfinished.txt'
+          with open(filename, 'w+') as f:
+            json.dump(pots, f)
+
+          filename = f'u_{n}_choose_{d}_lazy_bump_half_unfinished.txt'
+          with open(filename, 'w+') as f:
+            json.dump(ups, f)
+        
+        # print('Written')
+        break
+      except KeyboardInterrupt:
+        print("Exiting")
+        exiting = True
+        pass
+
+    if exiting:
+      break
+
+  print(final_status)
