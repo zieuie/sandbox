7665a27e01452d06f9aa2b5215d1e1fe5d763ff6
Zooey <truezooe@gmail.com>
2026-01-01 14:53:44 -0600

    Some speedup

--- transversals/haxell.py
+++ transversals/haxell.py
@@ -1,42 +1,35 @@
+import functools
 import itertools as it
 import math
+from collections import Counter, defaultdict
 from copy import deepcopy
-from typing import Any
 from datetime import datetime
-import functools
 
 
-VERBOSE = False
 def edge(self, other):
-  different_colors = False
-  for a,b in zip(self, other):
-    if abs(a-b) >= pa_distance:
+  i = 0
+  L = len(self)
+  while i < L:
+    if (self[i]-other[i])**2 >= dsquared:
       return False
-    elif a // pa_distance != b // pa_distance:
-      different_colors = True
-  return different_colors
+    i += 1
+  return True
 
 
-def separated(first, second):
-  for a,b in zip(first, second):
-    if abs(a-b) >= pa_distance:
-      return True
-  return False
-
-
-# page 19
-def find_it():
+def find_it(limit=None):
   global Ay0
-  # M[class] = vertex
+
+  limit = limit or len(colors)
   M = dict()
-  for i in it.count():
+  for i in range(limit):
     for A in colors:
       if A not in M:
         break
     else:
       break
 
-    print(datetime.now(), f'find_it iteration {i+1} of {len(colors)} |M| {len(M)}')
+    if not i % 100:
+      print(datetime.now(), f'find_it iteration {i} of {len(colors)}')
 
     Ay0 = A
     M = grow_transversal(M, A)
@@ -54,10 +47,9 @@ def grow_transversal(M, A):
   Ys = [dict()]
   l = 0
   while A not in M:
-    VERBOSE and print('*',end='',flush=True)
-    VERBOSE and print('[',l,end='',flush=True)
-    X,Y = build_layer(M,Xs,Ys,dict(),dict(),l)
-    VERBOSE and print(']',end='',flush=True)
+    forbidden = calc_forbidden(Xs,Ys,dict(),dict(),l)
+    colors = (Ys[l] if l else [Ay0])
+    X,Y = build_layer(M,dict(),dict(),forbidden,colors)
 
     Yllen = sum(map(len,Ys))
     if sum(map(len,X.values())) <= rho * Yllen:
@@ -78,7 +70,7 @@ def grow_transversal(M, A):
         return M
 
       to_delete = set()
-      for w_color, w in Ys[l-1].items():
+      for w_color in Ys[l-1]:
         if w_color not in Xs[l]:
           continue
 
@@ -92,47 +84,44 @@ def grow_transversal(M, A):
       for a in to_delete:
         Ys[l-1].pop(a)
 
-      Xs, Ys, l = superposed_build(M, Xs[:l], Ys[:l], l-1)
+      # superposed_build
+      Xs, Ys, l = Xs[:l], Ys[:l], l-1
+      for l in range(1,l+1):
+        forbidden = calc_forbidden(Xs,Ys,Xs[l],Ys[l],l-1)
+        colors = (Ys[l-1] if l-1 else [Ay0])
+        X,Y = build_layer(M,Xs[l],Ys[l],forbidden,colors)
+        if sum(map(len,X.values())) >= (1+mu)*sum(map(len,Xs[l].values())):
+          Xs[l], Ys[l] = X,Y
+          break
+      Xs, Ys = Xs[:l+1], Ys[:l+1]
 
 
-def superposed_build(M, Xs, Ys, l):
-  i = 1
-  while i <= l:
-    X, Y = build_layer(M,Xs,Ys,Xs[i],Ys[i],i-1)
-    if sum(map(len,X.values())) >= (1+mu)*sum(map(len,Xs[i].values())):
-      Xs[i], Ys[i] = X,Y
-      l = i
-    i += 1
-  return Xs[:l+1], Ys[:l+1], l
+def calc_forbidden(Xs,Ys,X,Y,l):
+  forbidden = defaultdict(set)
+  for a, v in Y.items():
+    forbidden[a].add(v)
+
+  for a, vs in X.items():
+    for v in vs:
+      forbidden[a].add(v)
+
+  for i in range(l+1):
+    for a, v in Ys[i].items():
+      forbidden[a].add(v)
+
+    for a, vs in Xs[i].items():
+      for v in vs:
+        forbidden[a].add(v)
+  return forbidden
 
 
-def build_layer(M,Xs,Ys,X,Y,l):
-  global Ay0
-  VERBOSE and print('a',end='',flush=True)
+def build_layer(M,X,Y,forbidden,colors):
   if X:
     X = {k:set(v) for k,v in X.items()}
   if Y:
     Y = {**Y}
-  VERBOSE and print('b',end='',flush=True)
 
-  forbidden = dict()
-  for a, v in Y.items():
-    forbidden.setdefault(a,set()).add(v)
-
-  for a, vs in X.items():
-    for v in vs:
-      forbidden.setdefault(a,set()).add(v)
-
-  for i in range(l+1):
-    for a, v in Ys[i].items():
-      forbidden.setdefault(a,set()).add(v)
-
-    for a, vs in Xs[i].items():
-      for v in vs:
-        forbidden.setdefault(a,set()).add(v)
-
-  for A in (Ys[l] if l else [Ay0]):
-    VERBOSE and print('c',end='',flush=True)
+  for A in colors:
     for v in from_color(A):
       if len(X.get(A, tuple())) >= U:
         break
@@ -141,59 +130,29 @@ def build_layer(M,Xs,Ys,X,Y,l):
         continue
 
       good = True
-      for na, ns in ident_neigh.items():
-        ua = tuple(na[src] for src in v)
-        if ua in forbidden:
-          for neigh in ns:
-            if tuple(neigh[src] for src in v) in forbidden[ua]:
-              good = False
-              break
+      for ua,us in forbidden.items():
+        if ua == A:
+          continue
+        for u in us:
+          if edge(u,v):
+            good = False
+            break
         if not good:
           break
-
       if not good:
         continue
 
       # X = X u {v}
       X.setdefault(A, set()).add(v)
+      forbidden[A].add(v)
 
       # Y = Y u {u in M | uv in E}
       for u_color, u in M.items():
-        if edge(v, u):
+        if A != u_color and edge(v, u):
           Y[u_color] = u
-          forbidden.setdefault(u_color,set()).add(u)
-
-  VERBOSE and print('c',end='',flush=True)
+          forbidden[u_color].add(u)
 
   return X,Y
-
-
-def is_good(A, v, Xs, Ys, X, Y, l):
-  if v == Y.get(A):
-    return False
-
-  if v == Ys[l].get(A):
-    return False
-
-  if A in X and v in X[A]:
-    return False
-
-  for neigh in ident_neigh:
-    u = tuple(neigh[src] for src in v)
-    u_color = tuple(e//pa_distance for e in u)
-    if u == Y.get(u_color):
-      return False
-
-    if u_color in X and u in X[u_color]:
-      return False
-
-    for i in range(l+1):
-      if u == Ys[i].get(u_color):
-        return False
-      if u_color in Xs[i] and u in Xs[i][u_color]:
-        return False
-
-  return True
 
 
 def immediate_count(M,W,l):
@@ -203,8 +162,8 @@ def immediate_count(M,W,l):
 
   for v_color, vs in W.items():
     for v in vs:
-      for u in M.values():
-        if edge(u, v):
+      for u_color, u in M.items():
+        if v_color != u_color and edge(u, v):
           break
       else:
         if l != 1 or v_color == Ay0:
@@ -215,17 +174,11 @@ def immediate_count(M,W,l):
   return Icount, Iany, Iany_color
 
 
-# @functools.lru_cache(maxsize=128)
-@functools.cache
+# @functools.lru_cache(maxsize=2)
 def from_color(A):
-  n,d = perm_len, pa_distance
-  groups = []
-  for x in range(int(math.ceil(n/d))):
-    groups.append(list(range(d*x, min(n, d*(x+1)))))
-
+  global cgroups
   ret = []
-  groups = list(map(list,map(it.permutations, groups)))
-  for g in it.product(*groups):
+  for g in it.product(*cgroups):
     g = list(map(iter, g))
     ret.append(tuple(next(g[e]) for e in A))
   return ret
@@ -302,9 +255,19 @@ def make_ident_neigh():
     pt = [e//d for e in p]
     if pt == qt:
       continue
-    if not separated(p, q):
+    if edge(p, q):
       neigh.setdefault(tuple(pt),set()).add(tuple(p))
   return neigh
+
+
+def make_foes():
+  n,d = perm_len, pa_distance
+  q = tuple(range(n))
+  qt = tuple(e//d for e in q)
+
+
+
+
 
 
 # globals
@@ -313,22 +276,41 @@ if __name__ == '__main__':
   perm_len = int(argv[1])
   pa_distance = int(argv[2])
   eps = float(argv[3]) if len(argv) > 3 else 0.1
+  dsquared = pa_distance**2
 
   print (f'P({perm_len}, {pa_distance}) with epsilon {eps}')
 
   ident_neigh = make_ident_neigh()
   r = len(ident_neigh)
   print(f'Degree {sum(map(len,ident_neigh))} and {r}-claw free')
+  print(sorted(Counter(map(len,ident_neigh.values())).items()))
 
   mu, U, rho = feasible_constants(r, eps)
   print(f'mu,U,rho are ', mu,U,rho)
 
   colors = list(make_colors())
   print(f'There are {len(colors)} colors')
-  for color in colors:
-    from_color(color)
+
+  # globals for from_color()
+  cgroups = []
+  for x in range(int(math.ceil(perm_len/pa_distance))):
+    cgroups.append(list(range(pa_distance*x, min(perm_len, pa_distance*(x+1)))))
+  cgroups = list(map(list,map(it.permutations, cgroups)))
+
+
+  # import cProfile
+
+  # pr = cProfile.Profile()
+  # pr.enable()  # Start profiling
 
   M = find_it()
+
+  # pr.disable()  # Stop profiling
+  # pr.print_stats(sort='tottime') # Print statistics, sorted by cumulative time
+  # exit(0)
+
+
+  # M = find_it()
 
   print('M is')
   for v in M.values():
